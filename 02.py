afd3333772da509e1760086fd657e503388443ef
Sezaltino <gabrielsezaltino@hotmail.com>
2025-11-25 01:14:27 -0300

    recode 02 exercise without bugs

--- 02.py
+++ 02.py
@@ -1,42 +1,30 @@
-def formatter(name):
-    name = name.casefold().strip().capitalize()
-    return name
-
 def normalize_name(names):
 
     names_filted = []
     names_filted2 = []
-    condition = False
 
-    #first solution
+    #first solution - not filter number caracter if has
     name_filted_strings = list(name.split() for name in names)
     for name in name_filted_strings:
         names_filted2.append(list(map(lambda x: x.casefold().strip().capitalize(), name)))
     names_filted2 = list(map(lambda x: " ".join(x), names_filted2))
 
-    #second solution
+    #second solution - better solution
     for name in names:
-        for letter in name:
-            if condition:
-                continue
-            condition = True if letter.isdigit() else False
-
-        if not condition:
-            #TODO rever algoritmo para identificar mais de 2 nomes (ideia full_name = name.split())
-            name = name.split()
-            first_name = name[0]
-            last_name = name[1]
-            names_filted.append(formatter(first_name)+ " " +formatter(last_name))
-
+        has_digit = any(letter.isdigit() for letter in name)
+        if not has_digit:
+            final_name = list(word.casefold().strip().capitalize() for word in name.split())
+            names_filted.append(" ".join(final_name))
         else:
             names_filted.append(f"name contain numeric caracter")    
+
     return names_filted
 
 if __name__ == "__main__":
     names = [
         "  Linda Sezaltino  ",
+        "PEDRO oliveir4  ",
         "joão SANTOS",
         "   Ana   Costa   ",
-        "PEDRO oliveir4  "
         ]
     print(normalize_name(names))
