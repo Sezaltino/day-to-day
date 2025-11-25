398052f99c1d8c89b87f3a98d618a1dd494fa7bc
Sezaltino <gabrielsezaltino@hotmail.com>
2025-11-24 23:44:07 -0300

    question 03 comment

---
+++ 03.py
@@ -0,0 +1,20 @@
+'''
+Dia 3 – Contagem de Palavras
+📋 Contexto
+Você está analisando feedbacks de clientes e precisa descobrir quais palavras aparecem com mais frequência para identificar padrões.
+🎯 Objetivo
+Crie uma função contar_palavras(texto) que receba um texto e retorne um dicionário com a contagem de cada palavra.
+📥 Entrada Esperada
+pythontexto = "python é legal python é poderoso e python é muito usado"
+📤 Saída Esperada
+python{
+    'python': 3,
+    'é': 3,
+    'legal': 1,
+    'poderoso': 1,
+    'e': 1,
+    'muito': 1,
+    'usado': 1
+}
+'''
+
